d0ee77a01d49a92f5135dc3192e8a837ce22b1cd
saurabhmishra-rgb <114287098+saurabhmishra-rgb@users.noreply.github.com>
2026-01-12 18:42:28 +0530

    Remove theme toggle component and update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import Theme from './components/Theme/theme.jsx'; // ✅ if default export
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/E-commerce/">
-    <Theme>
-      <App />
-    </Theme>
+    <App />
   </BrowserRouter>
 );
